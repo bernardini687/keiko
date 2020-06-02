b6f4f5cddc727e4f943eb199ea6ac064db8e9846
oscar bernardini <bernardini687@gmail.com>
2020-06-02 12:00:02 +0200

    add script for easy reset

---
+++ reset.sh
@@ -0,0 +1,15 @@
+rm dev.db &&
+vr prepare &&
+vr add i 1350 m &&
+vr add g 3 m &&
+vr add g 6 m &&
+vr add g 350 m &&
+vr add g 50 m &&
+vr add g 200 y &&
+vr add l 30 y &&
+vr add l 14 27-5-2020 &&
+vr add l 7,99 1-6-2020 &&
+vr add l 2,5 1-6-2020 &&
+vr add e 2,5 1-6-2020 &&
+vr add g 15,99 2-6-2020 &&
+vr add l 19,9 2-6-2020
