6774884ff676f73ccce3a502cccc31b2b2fd567f
oscar bernardini <bernardini687@gmail.com>
2020-06-02 14:05:37 +0200

    add and prepare are now functions

--- add.ts
+++ add.ts
@@ -2,18 +2,21 @@ import { isEntry, isRegular } from './helpers/validators.ts'
 import { buildEntry, buildRegular } from './helpers/builders.ts'
 import { insert } from './helpers/inserter.ts'
 
-const { args, exit } = Deno
+export async function add(args: string[]) {
+  if (isEntry(args)) {
+    await insert(...buildEntry(args))
+    exit()
+  }
 
+  if (isRegular(args)) {
+    await insert(...buildRegular(args))
+  } else {
+    console.log('TODO: help handling')
+    exit(1)
+  }
+}
+
+const { args, exit } = Deno
 console.log(args)
 
-if (isEntry(args)) {
-  await insert(...buildEntry(args))
-  exit()
-}
-
-if (isRegular(args)) {
-  await insert(...buildRegular(args))
-} else {
-  console.log('TODO: help handling')
-  exit(1)
-}
+await add(args)
--- prepare.ts
+++ prepare.ts
@@ -1,27 +1,31 @@
 import { db } from './helpers/provider.ts'
 import { save } from 'https://deno.land/x/sqlite@v1.0.0/mod.ts'
 
-db.query(
-  `
-  CREATE TABLE IF NOT EXISTS entries (
-    date TEXT NOT NULL,
-    amount INTEGER NOT NULL,
-    category TEXT NOT NULL
+export async function prepare() {
+  db.query(
+    `
+    CREATE TABLE IF NOT EXISTS entries (
+      date TEXT NOT NULL,
+      amount INTEGER NOT NULL,
+      category TEXT NOT NULL
+    )
+    `,
+    []
   )
-  `,
-  []
-)
 
-db.query(
-  `
-  CREATE TABLE IF NOT EXISTS regulars (
-    interval TEXT NOT NULL,
-    amount INTEGER NOT NULL,
-    category TEXT NOT NULL
+  db.query(
+    `
+    CREATE TABLE IF NOT EXISTS regulars (
+      interval TEXT NOT NULL,
+      amount INTEGER NOT NULL,
+      category TEXT NOT NULL
+    )
+    `,
+    []
   )
-  `,
-  []
-)
 
-await save(db)
-db.close()
+  await save(db)
+  db.close()
+}
+
+await prepare()
