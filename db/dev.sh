e1785ded513d00972883631801dec845f4ccc8ed
oscar bernardini <bernardini687@gmail.com>
2020-06-04 08:42:54 +0200

    sort review

--- db/dev.sh
+++ db/dev.sh
@@ -10,10 +10,10 @@ vr add g 50 m &&
 vr add g 200 y &&
 vr add l 30 y &&
 
-vr add l 14 27-5-2020 &&
 vr add l 7,99 1-6-2020 &&
 vr add l 2,5 1-6-2020 &&
-vr add e 2,5 1-6-2020 &&
-vr add g 15,99 2-6-2020 &&
 vr add l 19,9 2-6-2020 &&
+vr add e 2,5 1-6-2020 &&
+vr add l 14 27-5-2020 &&
+vr add g 15,99 2-6-2020 &&
 vr add c 11.99 2-6-2020
--- src/review/handler.ts
+++ src/review/handler.ts
@@ -3,35 +3,46 @@ import { formatAmt, formatDate } from '../shared/formatters.ts'
 import { db } from '../shared/provider.ts'
 
 export function review(table: 'regulars' | 'entries') {
-  const isRegulars = table === 'regulars'
-
-  const column = isRegulars ? 'interval' : 'date'
-
-  const rows = db.query(`SELECT ${column}, amount, category FROM ${table}`, [])
-
-  const records: any[] = []
-
-  if (isRegulars) {
-    // @ts-ignore
-    for (const [interval, amount, category] of rows) {
-      records.push({ interval, amount: formatAmt(amount), category })
-    }
-    console.log(
-      miniTable(records, ['interval', 'amount', 'category'], { upcaseHeader: true })
-    )
+  if (table === 'regulars') {
+    showRegulars()
   } else {
-    // @ts-ignore
-    for (const [date, amount, category] of rows) {
-      records.push({
-        date: formatDate(new Date(date)),
-        amount: formatAmt(amount),
-        category,
-      })
-    }
-    console.log(
-      miniTable(records, ['date', 'amount', 'category'], { upcaseHeader: true })
-    )
+    showEntries()
   }
 
   db.close()
+}
+
+function showRegulars(records: any[] = []) {
+  const rows = db.query(
+    `SELECT interval, amount, category FROM regulars ORDER BY interval DESC, amount ASC`,
+    []
+  )
+
+  //@ts-ignore
+  for (const [interval, amount, category] of rows) {
+    records.push({ interval, amount: formatAmt(amount), category })
+  }
+
+  console.log(
+    miniTable(records, ['interval', 'amount', 'category'], { upcaseHeader: true })
+  )
+}
+
+function showEntries(records: any[] = []) {
+  const rows = db.query(
+    `SELECT date, amount, category FROM entries ORDER BY date ASC`,
+    []
+  )
+
+  for (const [date, amount, category] of rows) {
+    records.push({
+      date: formatDate(new Date(date)),
+      amount: formatAmt(amount),
+      category,
+    })
+  }
+
+  console.log(
+    miniTable(records, ['date', 'amount', 'category'], { upcaseHeader: true })
+  )
 }
