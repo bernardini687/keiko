c2de27620635e4af6ccd1a2375ce57a4286f7781
oscar bernardini <bernardini687@gmail.com>
2020-06-07 13:55:39 +0200

    fix always error exit code

--- src/add/handler.ts
+++ src/add/handler.ts
@@ -5,9 +5,7 @@ import { isEntry, isRegular } from './helpers/validators.ts'
 export function add(args: string[]) {
   if (isEntry(args)) {
     insert(...buildEntry(args))
-  }
-
-  if (isRegular(args)) {
+  } else if (isRegular(args)) {
     insert(...buildRegular(args))
   } else {
     Deno.exit(1)
