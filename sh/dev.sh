82889e60982e03e760072aa707a566801a7b1fb4
oscar bernardini <bernardini687@gmail.com>
2020-08-03 08:59:51 +0200

    change db

--- sh/dev.sh
+++ sh/dev.sh
@@ -1,3 +1,5 @@
+# `dev` should match the env set by `scripts.yml`
+
 touch db/dev.db &&
 rm db/dev.db &&
 vr p &&
@@ -48,7 +50,7 @@ vr a l 3.3 12-7-2020 &&
 vr a l 8.5 12-7-2020 &&
 vr a l 12 16-7-2020 &&
 vr a l 7 18-7-2020 &&
-vr a e 10 18-7-2020 && 
+vr a e 10 18-7-2020 &&
 vr a c 9.19 19-7-2020 &&
 vr a l 25 21-7-2020 &&
 vr a l 5.5 22-7-2020 &&
@@ -59,4 +61,5 @@ vr a c 25 25-7-2020 &&
 vr a g 40.94 27-7-2020 &&
 vr a e 14 28-7-2020 &&
 vr a g 108.85 29-7-2020 &&
-vr a l 11 29-7-2020
+vr a l 11 29-7-2020 &&
+vr a l 5 31-7-2020
