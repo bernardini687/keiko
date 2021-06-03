4bb6b8f60cf0b2b7d282d31515628be9ce8f0d9b
oscar bernardini <bernardini687@gmail.com>
2021-06-03 08:21:04 +0200

    setup new db

--- src/shared/provider.ts
+++ src/shared/provider.ts
@@ -1,6 +1,6 @@
 import { DB } from '../deps.ts'
 
-const mode = Deno.env.get('DENO_ENV') ?? 'keiko'
+const mode = Deno.env.get('KEIKO_DB') ?? 'keiko'
 
 /** In-memory reference to the sqlite database file. */
 export const db = new DB(`${Deno.cwd()}/db/${mode}.db`)
